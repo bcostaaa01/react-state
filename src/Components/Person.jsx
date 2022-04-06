6f06c31b5c4eaf8665e5fb56ae50e7d03bf402f9
Bruno <bcostinha01@gmail.com>
2022-04-06 16:15:25 +0200

    First commit

---
+++ src/Components/Person.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import React from "react";
+
+const Person = () => {
+  const [person, setTraits] = useState({
+    talk: "talkative",
+    think: "thoughtful",
+  });
+
+  const updatePerson = () => {
+    setTraits((prevState) => {
+      return {
+        ...prevState,
+        talk: "silent",
+      };
+    });
+  };
+
+  const [laptop, setTechnicalDetails] = useState({
+      ram: '8GB',
+      brand: 'Lenovo',
+      color: 'black'
+  })
+
+  const updateLaptop = () => {
+      setTechnicalDetails((prevState) => {
+          return {
+              ...prevState,
+              brand: 'Apple',
+              color: 'Light grey'
+          };
+      });
+  };
+
+  const [woonplats, setWoonplats] = useState('Limburg');
+
+  const updateWoonplats = () => {
+      setWoonplats((woonplats) => {
+return woonplats = 'Den Bosch'
+      })
+  }
+
+  return (
+    <>
+      <p>I am {person.talk}</p>
+      <button onClick={updatePerson}>Update Person</button>
+      <p>I have a {laptop.color} {laptop.brand}</p>
+      <button onClick={updateLaptop}>Update Laptop</button>
+      <p>I live in {woonplats}</p>
+      <button onClick={updateWoonplats}>Update woonplats</button>
+    </>
+  );
+};
+
+const MemoizedComponent = React.memo(Person);
+
+export default MemoizedComponent;
